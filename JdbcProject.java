6e8323122c3da75a71025eb3b9d425c45ad58388
karl2603 <arvindrajkarl@gmail.com>
2026-03-27 18:50:04 +0530

    Add Feature

--- JdbcProject.java
+++ JdbcProject.java
@@ -12,7 +12,7 @@ public class JdbcProject {
             System.out.println("Database Connected");
             System.out.println("Enter the number");
             System.out.println("1.View Records");
-            System.out.println("2.Update Record");
+            System.out.println("2.Add Record");
             System.out.println("3.Delete Record");
             System.out.println("4.Exit Program");
             int choice = Input.nextInt();
@@ -24,6 +24,21 @@ public class JdbcProject {
                     System.out.println(
                             "Name : " + rs.getString(2) + ", Role : " + rs.getString(3) + ", Salary : " + rs.getInt(4));
                 }
+            }
+            else if(choice == 2){
+                Input.nextLine();
+                System.out.println("Enter Employee Name : ");
+                String name = Input.nextLine();
+                System.out.println("Enter Employee Role : ");
+                String role = Input.nextLine();
+                System.out.println("Enter Employee Salary : ");
+                int salary = Input.nextInt();
+                String addQuery = "insert into Employee (name, role, salary) values (?,?,?)";
+                PreparedStatement st = con.prepareStatement(addQuery);
+                st.setString(1, name);
+                st.setString(2, role);
+                st.setInt(3, salary);
+                st.executeUpdate();
             }
         } catch (Exception e) {
             System.out.println("Unable to connect to Database");
